fe84b8802ac70168fdc1da4356d58a1d2d51a127
Arthur Novello <novelloarthur@gmail.com>
2019-09-12 18:24:26 -0300

    configuração do db

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,45 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_user import login_required, UserManager, UserMixin, SQLAlchemyAdapter
+from flask_mail import Mail
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://flask:flaskpwd@localhost:3307/testDB'
+
+app.config['USER_APP_NAME'] = 'MyApp'
+app.config['SECRET_KEY'] = 'thisisasecret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost:3307/testDB'
+app.config['CSRF_ENABLE'] = True
+app.config['USER_ENABLE_EMAIL'] = True
+app.config['USER_EMAIL_SENDER'] = app.config['USER_APP_NAME']
+app.config.from_pyfile('config.cfg')
+
 db = SQLAlchemy(app)
+mail = Mail(app)
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False, server_default=' ')
+    active = db.Column(db.Boolean(), nullable=False, server_default='0')
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    confirmed_at = db.Column(db.DateTime())
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+
+# Setup Flask-User
+user_manager = UserManager(app, db, User)
+
+
+@app.route('/')
+def index():
+    return '<h1>Home</h1>'
+
+
+@app.route('/profile')
+@login_required
+def profile():
+    return '<h1>Protect</h1>'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
