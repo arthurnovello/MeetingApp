380f24f2d3b7d49b56cfccb1f4d7920cf501d57d
Arthur Novello <novelloarthur@gmail.com>
2019-09-14 13:58:12 -0300

    Configurando flask-user
    Adicionando canfirmação do email

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_user import login_required, UserManager, UserMixin, SQLAlchemyAdapter
+from flask_user import \
+        login_required, UserManager, UserMixin
 from flask_mail import Mail
 
 app = Flask(__name__)
 
 app.config['USER_APP_NAME'] = 'MyApp'
 app.config['SECRET_KEY'] = 'thisisasecret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost:3307/testDB'
+app.config['SQLALCHEMY_DATABASE_URI'] \
+        = 'mysql+pymysql://root:root@localhost:3307/testDB'
 app.config['CSRF_ENABLE'] = True
 app.config['USER_ENABLE_EMAIL'] = True
 app.config['USER_EMAIL_SENDER'] = app.config['USER_APP_NAME']
@@ -23,7 +25,7 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(255), nullable=False, server_default=' ')
     active = db.Column(db.Boolean(), nullable=False, server_default='0')
     email = db.Column(db.String(255), nullable=False, unique=True)
-    confirmed_at = db.Column(db.DateTime())
+    email_confirmed_at = db.Column(db.DateTime())
 
 
 # Setup Flask-User
